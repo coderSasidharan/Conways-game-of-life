1b5b272c0a871f6b8fa304b45af0aaf221d7fb88
coderSasidharan <66076670+coderSasidharan@users.noreply.github.com>
2023-09-29 11:01:45 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,40 @@
+import java.io.*;
+import java.util.*;
 
+class Main {
+  public static void main(String[] args) throws Exception{
+    
+    System.out.println("\nConway's Game of Life\n\n");
+    System.out.println("Enter File Name: ");
+
+    Scanner input = new Scanner(System.in);
+    File file = new File(input.nextLine());
+    Scanner scanner = new Scanner(file);
+    System.out.println("Enter the number of generations: ");
+    int numGen = input.nextInt();
+    
+    int[] gridX = new int[100];
+    int[] gridY = new int[100];
+    int x = 0;
+    int y = 0;
+
+    
+    for(int i = 0; i<100; i++){
+      if(scanner.hasNext()){
+        gridX[i] = scanner.nextInt();
+        gridY[i] = scanner.nextInt();
+      }
+    }
+    
+    Gameboard initial = new Gameboard(gridX,gridY);
+
+    for(int i = 0; i<numGen;i++){
+      Thread.sleep(1000);
+      Cell newGame = new Cell(initial.returnGrid());
+      Gameboard nextGame  = new Gameboard(newGame.genCells());
+      initial = nextGame;
+
+    }
+    
+  }
+}
